2967d9c93c00542ba637dd7e95357abd93c1b90b
aszkid <polgomezriquelme@gmail.com>
2017-11-26 01:36:14 -0600

    Testing spdlog

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
+#include "spdlog/spdlog.h"
+
 #include "core.hpp"
 
 int main(int argc, char** argv)
 {
 	MilSim::Core core;
 
-	std::cout << "Hello world! MilSim version `"
-		<< core.version << "`"
-		<< std::endl;
+	auto console = spdlog::stdout_color_mt("console");
+	console->info("MilSim version `{}`", core.version);
+	console->warn("Easy padding in numbers like {:08d}", 12);
+	console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
 	
 	return 0;
 }
