f172ef3bf10fe064692f45cebb8ceba8eb49c44c
aszkid <polgomezriquelme@gmail.com>
2017-11-26 01:56:35 -0600

    Testing Lua

--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,8 @@
 
 #include "spdlog/spdlog.h"
 
+#include "selene.h"
+
 #include "core.hpp"
 
 int main(int argc, char** argv)
@@ -12,6 +14,10 @@ int main(int argc, char** argv)
 	console->info("MilSim version `{}`", core.version);
 	console->warn("Easy padding in numbers like {:08d}", 12);
 	console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+	
+	sel::State state;
+	state("x = 5 + 2");
+	console->info("Lua says that `x={}`", (int)state["x"]);
 	
 	return 0;
 }
