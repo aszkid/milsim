5223406c2a999c20de8f2fe242f02f895bdf2244
aszkid <polgomezriquelme@gmail.com>
2017-11-26 02:51:49 -0600

    Using `args`

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 
 #include "spdlog/spdlog.h"
-
 #include "selene.h"
+#include "args.hxx"
 
 #include "core.hpp"
 
@@ -11,13 +12,23 @@ int main(int argc, char** argv)
 	MilSim::Core core;
 
 	auto console = spdlog::stdout_color_mt("console");
-	console->info("MilSim version `{}`", core.version);
-	console->warn("Easy padding in numbers like {:08d}", 12);
-	console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
 	
 	sel::State state;
 	state("x = 5 + 2");
 	console->info("Lua says that `x={}`", (int)state["x"]);
+	
+	args::ArgumentParser parser("MilSim");
+	args::ValueFlag<std::string> root(parser, "root", "The root directory", {'r', "root"});
+	
+	try {
+		parser.ParseCLI(argc, argv);
+	} catch(args::ParseError e) {
+		console->error(e.what());
+	}
+	
+	if(root) {
+		console->info("Root is `{}`", args::get(root));
+	}
 	
 	return 0;
 }
